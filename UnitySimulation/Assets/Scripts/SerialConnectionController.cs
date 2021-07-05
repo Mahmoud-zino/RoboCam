4a641dd704a12fe8d501733c981093b82d7544e1
LWNW2Klwnzim3 <Mahmoud.Zino@liebherr.com>
2021-07-05 19:02:04 +0200

    minor SerialConnection Changes

--- UnitySimulation/Assets/Scripts/SerialConnectionController.cs
+++ UnitySimulation/Assets/Scripts/SerialConnectionController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System.IO.Ports;
+using TMPro;
 
 public class SerialConnectionController : MonoBehaviour
 {
@@ -19,5 +20,38 @@ public class SerialConnectionController : MonoBehaviour
     public List<string> GetAvailablePorts()
     {
         return SerialPort.GetPortNames().ToList();
+    }
+
+    public void TriggerConnection(TextMeshPro connect, TextMeshPro disconnect, TMP_Dropdown dropdown)
+    {
+        //Connect
+        if(connect.gameObject.activeSelf)
+        {
+            this.serialPort = new SerialPort(dropdown.options[dropdown.value].text, 9600, Parity.None, 8, StopBits.One);
+            this.serialPort.Open();
+            if(this.serialPort.IsOpen)
+            {
+                connect.gameObject.SetActive(false);
+                disconnect.gameObject.SetActive(true);
+            }
+            //Error opening serial connection
+            else
+            {
+                Debug.LogError("Error Opening the port");
+            }
+        }
+        //Disconnect
+        else
+        {
+            this.serialPort.Close();
+            connect.gameObject.SetActive(true);
+            disconnect.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (this.serialPort != null && this.serialPort.IsOpen)
+            this.serialPort.Close();
     }
 }
