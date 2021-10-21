aec2cfcb7c4a44b1c6a0b67b76d3ff0e34a85c01
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2021-10-21 11:27:09 +0200

    Added Mouse Camera Movement instead of Keyboard

--- UnitySimulation/Assets/Scripts/Movement/CamController.cs
+++ UnitySimulation/Assets/Scripts/Movement/CamController.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
-    [SerializeField] private float roataionSpeed = 100;
+    [SerializeField] private float roataionSpeed = 5;
 
-    private void LateUpdate()
+    public void OnDrag()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        this.transform.eulerAngles += new Vector3(verticalInput * roataionSpeed * Time.deltaTime, horizontalInput * roataionSpeed * Time.deltaTime, 0);
+        if (Input.GetMouseButton(0))
+        {
+            this.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * roataionSpeed, -Input.GetAxis("Mouse X") * roataionSpeed, 0));
+
+            if (this.transform.rotation.eulerAngles.x > 50 && this.transform.rotation.eulerAngles.x < 100)
+                this.transform.rotation = Quaternion.Euler(50, transform.rotation.eulerAngles.y, 0);
+            else if (this.transform.rotation.eulerAngles.x < 300 && this.transform.rotation.eulerAngles.x > 200)
+                this.transform.rotation = Quaternion.Euler(300, transform.rotation.eulerAngles.y, 0);
+            else
+                this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+        }
     }
 }
