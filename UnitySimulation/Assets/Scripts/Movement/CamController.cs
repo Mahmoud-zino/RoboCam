34531f6a85901785816c29a8131167efcf543468
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2021-10-21 10:27:39 +0200

    Fixed Camera Angle Error

--- UnitySimulation/Assets/Scripts/Movement/CamController.cs
+++ UnitySimulation/Assets/Scripts/Movement/CamController.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
-    [SerializeField]
-    private float roataionSpeed = 50;
-    [SerializeField]
-    private float movementSpeed = 100;
+    [SerializeField] private float roataionSpeed = 100;
 
-    private void Update()
+    private void LateUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        this.transform.Rotate(horizontalInput * roataionSpeed * Time.deltaTime * Vector3.up);
-
         float verticalInput = Input.GetAxis("Vertical");
-        this.transform.Translate(verticalInput * movementSpeed * Time.deltaTime * Vector3.forward);
+        this.transform.eulerAngles += new Vector3(verticalInput * roataionSpeed * Time.deltaTime, horizontalInput * roataionSpeed * Time.deltaTime, 0);
     }
 }
