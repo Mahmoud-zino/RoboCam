01c8c3b4e605971c3e337c890444e0f1bc52934e
Simon Masooglu <masooglu.simon@gmail.com>
2021-10-24 14:48:57 +0200

    Added python autostart script

---
+++ PythonCam/FaceRecognitionStartup.py
@@ -0,0 +1,3 @@
+import os
+while 1:
+    os.system(f"sudo /usr/bin/python3 -u /home/pi/Desktop/FaceRecognition.py -ip 192.168.2.10 -port 8000 -api http://192.168.2.10:5000/api | sudo tee /var/log/face.log")
